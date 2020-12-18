afb65938fd4a2f20044d66de4fa12f50d71f5b52
ihor13 <igor.gavrilchuk.3@mail.ru>
2020-12-18 05:48:49 +0200

    Todoist Building Task component

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,7 @@ import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
 
     useEffect(() => {
         let unsubscribe = firebase
@@ -21,7 +22,46 @@ export const useTasks = selectedProject => {
         : unsubscribe;
 
         unsubscribe =unsubscribe.onSnapshot(snapshot => {
+            const newTasks = snapshot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
 
+            setTasks(
+                selectedProject === 'NEXT_7'
+                ? newTasks.filter(
+                    task => moment(task.date,'DD-MM-YYYY').diff(moment(),'days') <= 7 &&
+                    task.archived !== true
+                )
+                : newTasks.filter(task => task.archived !== true)
+            );
+
+            setArchivedTasks(newTasks.filter(task => task.archived !== false));
         });
+
+        return () => unsubscribe();
     }, [selectedProject]);
+
+    return { tasks, archivedTasks };
+};
+
+export const useProjects = () => {
+    const [projects, setProjects] = setState([]);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection('projects')
+            .where('userId','==','931b49da0b434127')
+            .orderBy('projectId')
+            .get()
+            .then(snapshot => {
+                const allProjects = snapshot.docs.map(project =>({
+                    ...project.data(),
+                    docId: project.id,
+                }));
+
+                if (JSON.stringify(allProjects) !== JSON.stringify(projects))
+            });
+    }, [projects])
 };
