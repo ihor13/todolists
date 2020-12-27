44dca23b4037559fcd8fc4414f2ec2114dc0c07c
ihor13 <igor.gavrilchuk.3@mail.ru>
2020-12-28 01:31:09 +0200

    Todoist Styling application

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaPizzaSlice } from 'react-icons/fa'
+import { FaPizzaSlice } from 'react-icons/fa';
+
 export const Header = () => {
     return(
         <header className="header" data-testid="header">
@@ -9,8 +10,8 @@ export const Header = () => {
                 </div>
                 <div className="settings">
                     <ul>
-                       <li>+</li> 
-                       <li>
+                       <li data-testid="quick=add-task-action" className="settings__add">+</li> 
+                       <li data-testid="dark-mode-action" className="settings__darkmode">
                            <FaPizzaSlice/>
                        </li>
                     </ul>
